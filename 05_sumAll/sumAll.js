ff31ec57c3626dda6c6689a283cdeb2bf4b3b35f
MultiFlex <wakanawardrobe@gmail.com>
2022-03-25 14:41:03 +0000

    add if statement for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(num1,num2) {
     let sumTotal = 0;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1 == NaN || num2 == NaN) {
+        return "ERROR";
+    }
     for(i = num1; i <= num2; i++) {
         sumTotal+=i;
     }
