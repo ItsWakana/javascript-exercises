f35ce232ca55fac9b7a26dafe1276b24eb4c13c3
MultiFlex <wakanawardrobe@gmail.com>
2022-03-25 16:18:14 +0000

    add variable for calculation
    I also used toFixed method to round the decimal place down and converted the string into a number with the parsefloat method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(far) {
+  let result = (far - 32) * 5/9;
+  result.toFixed(0);
+  return Number.parseFloat(result);
 
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let result = (cel * 9 / 5) + 32;
+  result.toFixed(0);
+  return Number.parseFloat(result);
 };
 
 // Do not edit below this line
