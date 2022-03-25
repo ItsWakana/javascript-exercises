c02f624c6029c84bf3780f1a3f2259fa1147c84b
MultiFlex <wakanawardrobe@gmail.com>
2022-03-25 16:21:50 +0000

    set variable for the tofixed method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,14 @@
 const ftoc = function(far) {
   let result = (far - 32) * 5/9;
-  result.toFixed(0);
-  return Number.parseFloat(result);
+  let decimalResult = result.toFixed(1);
+  return Number.parseFloat(decimalResult);
 
 };
 
 const ctof = function(cel) {
   let result = (cel * 9 / 5) + 32;
-  result.toFixed(0);
-  return Number.parseFloat(result);
+  let decimalResult = result.toFixed(1);
+  return Number.parseFloat(decimalResult);
 };
 
 // Do not edit below this line
