a749aaad100a6fb80747a42af6d88e6d5e60dec0
MultiFlex <wakanawardrobe@gmail.com>
2022-03-25 15:09:49 +0000

    add if statement if objects are parsed as argument

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,6 +4,8 @@ const sumAll = function(num1,num2) {
         return "ERROR";
     } else if (typeof num1 === "string" || typeof num2 === "string") {
         return "ERROR";
+    } else if (typeof num1 === "object" || typeof num2 === "object") {
+        return "ERROR";
     }
     for(i = num1; i <= num2; i++) {
         sumTotal+=i;
