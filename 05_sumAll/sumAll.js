00175dd266490e0494e7171390f054c2e0d940f4
MultiFlex <wakanawardrobe@gmail.com>
2022-03-25 13:38:38 +0000

    Write function for adding numbers in between
    Had some trouble in that the total sum was not being returned correctly, this happened because the addition of each number in-betweewn wasn't calculating correctly. Turns out I just needed to add += instead of just +

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sumTotal = 0;
+    for(i = num1; i <= num2; i++) {
+        sumTotal+=i;
+    }
+    return sumTotal;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
