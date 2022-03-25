4260655a22b802c9c99cd8153c11a71bcd0cac1e
MultiFlex <wakanawardrobe@gmail.com>
2022-03-25 13:44:25 +0000

    Add 2nd for loop after the first one
    This 2nd for loop was in cases when the larger number came first in the process.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,10 @@ const sumAll = function(num1,num2) {
     for(i = num1; i <= num2; i++) {
         sumTotal+=i;
     }
+
+    for(i = num1; i>=num2; i--) {
+        sumTotal+=i;
+    }
     return sumTotal;
 };
 
